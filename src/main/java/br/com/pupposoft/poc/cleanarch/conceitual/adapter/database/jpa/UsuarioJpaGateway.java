496ed60437c27b67fc7284782df58cc0ff2cdb39
Renan Furtado <renan.furtado@wedotec.com.br>
2024-11-05 15:56:35 -0300

    Adicionando config

--- src/main/java/br/com/pupposoft/poc/cleanarch/conceitual/adapter/database/jpa/UsuarioJpaGateway.java
+++ src/main/java/br/com/pupposoft/poc/cleanarch/conceitual/adapter/database/jpa/UsuarioJpaGateway.java
@@ -3,6 +3,8 @@ package br.com.pupposoft.poc.cleanarch.conceitual.adapter.database.jpa;
 import java.util.List;
 import java.util.Optional;
 
+import org.springframework.stereotype.Component;
+
 import br.com.pupposoft.poc.cleanarch.conceitual.adapter.database.jpa.entity.UsuarioEntity;
 import br.com.pupposoft.poc.cleanarch.conceitual.adapter.database.jpa.repository.UsuarioRepository;
 import br.com.pupposoft.poc.cleanarch.conceitual.core.domain.Automovel;
@@ -13,44 +15,45 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
+@Component
 @RequiredArgsConstructor
 public class UsuarioJpaGateway implements UsuarioGateway {
 
 	private final UsuarioRepository usuarioRepository;
-	
+
 	@Override
 	public Optional<Usuario> obterPorCpf(String cpf) {
 		try {
-			
+
 			Optional<UsuarioEntity> usuarioEntityOp = usuarioRepository.findByCpf(cpf);
-			
+
 			if(usuarioEntityOp.isEmpty()) {
 				log.info("Usuário não encontrado: cpf={}", cpf);
 				return Optional.empty();
 			}
-			
+
 			UsuarioEntity usuarioEntity = usuarioEntityOp.get();
-			
+
 			Usuario usuario = mapToDomain(usuarioEntity);
-			
+
 			return Optional.of(usuario);
-			
-			
+
+
 		} catch (Exception e) {
 			log.error(e.getMessage(), e);
 			throw new ErroAoAcessarRepositorioException();
 		}
-		
+
 	}
 
 	@Override
 	public Long criar(Usuario usuario) {
 		try {
-			
+
 			UsuarioEntity usuarioEntity = mapToEntity(usuario);
-			
+
 			return usuarioRepository.save(usuarioEntity).getId();
-			
+
 		} catch (Exception e) {
 			log.error(e.getMessage(), e);
 			throw new ErroAoAcessarRepositorioException();
@@ -58,28 +61,27 @@ public class UsuarioJpaGateway implements UsuarioGateway {
 	}
 
 	private Usuario mapToDomain(UsuarioEntity usuarioEntity) {
-		List<Automovel> automoveis = usuarioEntity.getAutomoveis().stream().map(ae -> {
-			return new Automovel(
-					ae.getId(),
-					ae.getModelo(),
-					ae.getDataModelo(),
-					null);
-		}).toList();
-		
+		List<Automovel> automoveis = usuarioEntity.getAutomoveis().stream().map(ae -> 
+		new Automovel(
+				ae.getId(),
+				ae.getModelo(),
+				ae.getDataModelo(),
+				null)).toList();
+
 		return new Usuario(
-						usuarioEntity.getId(), 
-						usuarioEntity.getCpf(), 
-						usuarioEntity.getNome(), 
-						usuarioEntity.getDataNascimento(), 
-						automoveis);
+				usuarioEntity.getId(), 
+				usuarioEntity.getCpf(), 
+				usuarioEntity.getNome(), 
+				usuarioEntity.getDataNascimento(), 
+				automoveis);
 	}
-	
+
 	private UsuarioEntity mapToEntity(Usuario usuario) {
 		return UsuarioEntity.builder()
 				//FIXME: implementar
-				
+
 				.build();
 	}
-	
+
 
 }
---
+++ src/main/java/br/com/pupposoft/poc/cleanarch/conceitual/config/InjecaoDependenciaConfiguration.java
@@ -0,0 +1,24 @@
+package br.com.pupposoft.poc.cleanarch.conceitual.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import br.com.pupposoft.poc.cleanarch.conceitual.core.gateway.UsuarioGateway;
+import br.com.pupposoft.poc.cleanarch.conceitual.core.usecase.CriarUsuarioUsecase;
+import br.com.pupposoft.poc.cleanarch.conceitual.core.usecase.CriarUsuarioUsecaseImpl;
+import lombok.RequiredArgsConstructor;
+
+@Configuration
+@RequiredArgsConstructor
+public class InjecaoDependenciaConfiguration {
+
+	private UsuarioGateway usuarioGateway;
+	
+	@Bean
+	public CriarUsuarioUsecase criarUsuarioUsecase() {
+		return new CriarUsuarioUsecaseImpl(usuarioGateway);
+	}
+
+	//Criar demais usecases, etc
+	
+}
