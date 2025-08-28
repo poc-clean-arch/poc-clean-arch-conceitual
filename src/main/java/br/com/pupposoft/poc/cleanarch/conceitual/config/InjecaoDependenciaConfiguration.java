8eda8560bedbcd4220b938cf949f43df197e7a98
Renan Furtado <renan.furtado@wedotec.com.br>
2025-08-28 11:27:38 -0300

    Removendo libs incorretas

--- src/main/java/br/com/pupposoft/poc/cleanarch/conceitual/config/InjecaoDependenciaConfiguration.java
+++ src/main/java/br/com/pupposoft/poc/cleanarch/conceitual/config/InjecaoDependenciaConfiguration.java
@@ -1,24 +1,39 @@
 package br.com.pupposoft.poc.cleanarch.conceitual.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.DependsOn;
 
+import br.com.pupposoft.poc.cleanarch.conceitual.core.gateway.InfracaoGateway;
 import br.com.pupposoft.poc.cleanarch.conceitual.core.gateway.MotoristaGateway;
+import br.com.pupposoft.poc.cleanarch.conceitual.core.gateway.NotificacaoGateway;
 import br.com.pupposoft.poc.cleanarch.conceitual.core.usecase.CriarUsuarioUsecase;
 import br.com.pupposoft.poc.cleanarch.conceitual.core.usecase.CriarUsuarioUsecaseImpl;
+import br.com.pupposoft.poc.cleanarch.conceitual.core.usecase.ObterCalculadoraMultaUsecase;
 import lombok.RequiredArgsConstructor;
 
 @Configuration
 @RequiredArgsConstructor
 public class InjecaoDependenciaConfiguration {
 
-	private MotoristaGateway usuarioGateway;
+	private final MotoristaGateway motoritaGateway;
+	private final InfracaoGateway infracaoGateway;
+	private final NotificacaoGateway notificacaoGateway;	
 	
 	@Bean
-	public CriarUsuarioUsecase criarUsuarioUsecase() {
-		return new CriarUsuarioUsecaseImpl(usuarioGateway);
+	@Autowired
+	@DependsOn("obterCalculadoraMultaUsecase")
+	public CriarUsuarioUsecase criarUsuarioUsecase(ObterCalculadoraMultaUsecase obterCalculadoraMultaUsecase) {
+		return new CriarUsuarioUsecaseImpl(
+				motoritaGateway,
+				infracaoGateway,
+				obterCalculadoraMultaUsecase,
+				notificacaoGateway);
 	}
 
-	//Criar demais usecases, etc
-	
+	@Bean
+	public ObterCalculadoraMultaUsecase obterCalculadoraMultaUsecase() {
+		return new ObterCalculadoraMultaUsecase();
+	}
 }
--- src/main/java/br/com/pupposoft/poc/cleanarch/conceitual/core/mapper/MotoristaMapper.java
+++ src/main/java/br/com/pupposoft/poc/cleanarch/conceitual/core/mapper/MotoristaMapper.java
@@ -1,11 +1,8 @@
 package br.com.pupposoft.poc.cleanarch.conceitual.core.mapper;
 
-import org.springframework.stereotype.Component;
-
 import br.com.pupposoft.poc.cleanarch.conceitual.core.domain.Motorista;
 import br.com.pupposoft.poc.cleanarch.conceitual.core.dto.CriarUsuarioInputDto;
 
-@Component
 public class MotoristaMapper {
 
 	public Motorista map(CriarUsuarioInputDto dto) {
